ce1f1227a0c08a1af47dc5765c6c9533ab24a1ab
AarushDoshi11 <76525613+AarushDoshi11@users.noreply.github.com>
2021-01-04 20:34:24 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,26 +1,34 @@
-
+var garden,gardenImage;
+var jerry1,jerry1Image;
+var tom1,tom1Image
 function preload() {
-    //load the images here
+    gardenImage = loadImage("garden.png");
+    jerry1Image = loadImage("jerry1.png");
+    tom1Image = loadImage("tom1.png");
 }
 
 function setup(){
     createCanvas(1000,800);
-    //create tom and jerry sprites here
+    
+    garden = createSprite(50,315,20,20);
+    jerry1 = createSprite(600, 300 , 20, 20);
+    jerry1.scale = 0.2;
 
+    tom1 = createSprite(200, 300, 20,20);
+    tom1.scale = 0.2;
 }
 
 function draw() {
 
     background(255);
-    //Write condition here to evalute if tom and jerry collide
-
+   
+ keyPressed();
     drawSprites();
-}
-
+} 
 
 function keyPressed(){
-
-  //For moving and changing animation write code here
-
-
+if(keyCode === LEFT_ARROW){}
+tom1.velocity =  -5 ;
+tom1.addAnimation("tom1Running", tom1Image1);
+tom1.changeAnimation("catRunning");
 }
